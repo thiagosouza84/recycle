e4ea16469ae776789bf0bf3120f84a498ef07957
Lucas Melo <lucas.lljm@gmail.com>
2024-10-14 23:29:58 -0300

    undo changes

--- src/main/java/com/fiap/recycle/RecycleApplication.java
+++ src/main/java/com/fiap/recycle/RecycleApplication.java
@@ -2,8 +2,6 @@ package com.fiap.recycle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class RecycleApplication {
@@ -12,11 +10,4 @@ public class RecycleApplication {
 		SpringApplication.run(RecycleApplication.class, args);
 	}
 
-	@RestController
-	public static class HelloController {
-		@GetMapping("/hello")
-		public String hello() {
-			return "Hello, Recycle!";
-		}
-	}
 }
--- src/test/java/com/fiap/recycle/RecycleApplicationTests.java
+++ src/test/java/com/fiap/recycle/RecycleApplicationTests.java
@@ -1,32 +1,13 @@
 package com.fiap.recycle;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class RecycleApplicationTests {
-
-	@LocalServerPort
-	private int port;
-
-	@Autowired
-	private TestRestTemplate restTemplate;
+@SpringBootTest
+class RecycleApplicationTests {
 
 	@Test
-	public void contextLoads() throws Exception {
-		// Testa se o contexto carrega corretamente
-		assertThat(restTemplate).isNotNull();
+	void contextLoads() {
 	}
 
-	@Test
-	public void testHelloEndpoint() throws Exception {
-		// Testa se o endpoint /hello retorna o valor esperado
-		String url = "http://localhost:" + port + "/hello";
-		String response = this.restTemplate.getForObject(url, String.class);
-		assertThat(response).isEqualTo("Hello, Recycle!");
-	}
 }
